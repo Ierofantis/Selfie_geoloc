6150ef06e7239ca4caf0af1576632387e2550af3
Ierofantis <evil69669@gmail.com>
2017-10-07 14:58:59 +0300

    pixels

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -63,7 +63,7 @@ angular.module('qcApp')
 
     $scope.qualityFunc = function() {          
 
-      context.drawImage(video, 0, 0, 600, 540);
+      context.drawImage(video, 0, 0, 1024, 768);
       $scope.dataLoading = true;
       $scope.showVideo = false;
 
